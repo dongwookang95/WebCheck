d7dfd9f17420491a1308ae3aaa22cbf9b30fc890
dongwookang <d.kang-1@student.tudelft.nl>
2019-08-24 17:04:44 +0200

    add senarios

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 (async ()=> {
     const browser = await puppeteer.launch({
@@ -11,8 +12,42 @@ const puppeteer = require('puppeteer');
         
         });
     });
-    console.log(brandNames);
     
 
     await browser.close();
+    
+    // scenario 1: check if file exists
+    let contents
+    try {
+        contents = fs.readFileSync("brandname.txt", {
+            encoding: 'utf8'
+        });
+    } catch (err) {
+        // file does not exist
+        console.log('File does not exist; writing new file')
+        fs.writeFileSync("brandname.txt", brandNames.toString());
+        return
+    }
+
+    // scenario 2: file exists, same contents
+    if(contents === brandNames.toString()){
+        console.log('contents are the same');
+        return 
+    }
+    if(contents !== brandNames.toString()){
+        //To do send notification.
+        console.log('contents are different');
+        fs.writeFileSync("brandname.txt", brandNames.toString());
+    }
 })();
+
+
+/*
+1. no file
+    - write array to new file
+2. file exists, same contents
+    - do nothing
+3. file exists, different contents
+    - send notification
+    - overwrite with new content
+*/
