4444172aa427cbd4808d60611e59822891d0ccce
dongwookang <d.kang-1@student.tudelft.nl>
2019-09-04 20:30:26 +0200

    fix mistake

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const accountSid = 'AC4ba661edab5f347199fad6e5f86c167a'; 
+const accountSid = abc; 
 const authToken = abc;
 const client = require('twilio')(accountSid, authToken); 
 
